c1af2d728f1954c691521b33a8d230046665bf8b
jekuaitk <jekua@aarhus.dk>
2021-07-09 14:46:51 +0200

    Applied coding standards

--- src/Event/AccessControlEvent.php
+++ src/Event/AccessControlEvent.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace ItkDev\AdgangsstyringBundle\Event;
-
 
 use Symfony\Contracts\EventDispatcher\Event;
 
--- src/Util/UserManager.php
+++ src/Util/UserManager.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace ItkDev\AdgangsstyringBundle\Util;
-
 
 class UserManager
 {
