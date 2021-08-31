02a5ef3965e28598e90d6854e5e277037b5c4c4d
jekuaitk <jekua@aarhus.dk>
2021-08-31 12:26:52 +0200

    Cleaned up DeleteUserEvent

--- src/Event/DeleteUserEvent.php
+++ src/Event/DeleteUserEvent.php
@@ -6,18 +6,33 @@ use Symfony\Contracts\EventDispatcher\Event;
 
 class DeleteUserEvent extends Event
 {
-    private $data;
+    private array $data;
 
+    /**
+     * DeleteUserEvent constructor.
+     *
+     * @param array $data
+     */
     public function __construct(array $data)
     {
         $this->data = $data;
     }
 
+    /**
+     * Gets data
+     *
+     * @return array
+     */
     public function getData()
     {
         return $this->data;
     }
 
+    /**
+     * Sets data
+     *
+     * @param array $data
+     */
     public function setData(array $data)
     {
         $this->data = $data;
