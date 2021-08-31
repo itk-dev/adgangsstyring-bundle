aeda0cf82f3b4959cb8a3dedf0e0287674dfabc3
jekuaitk <jekua@aarhus.dk>
2021-08-31 13:27:54 +0200

    Used lock to ensure only one instance of delta sync is running at a time

--- src/Command/AccessControlCommand.php
+++ src/Command/AccessControlCommand.php
@@ -10,10 +10,12 @@ use ItkDev\AzureAdDeltaSyncBundle\Handler\UserHandler;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Lock\LockFactory;
 
 class AccessControlCommand extends Command
 {
     private Controller $controller;
+    private LockFactory $factory;
     private UserHandler $handler;
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'delta-sync:run';
@@ -22,11 +24,13 @@ class AccessControlCommand extends Command
      * AccessControlCommand constructor.
      *
      * @param Controller $controller
+     * @param LockFactory $factory
      * @param UserHandler $handler
      * @param string|null $name
      */
-    public function __construct(Controller $controller, UserHandler $handler, string $name = null)
+    public function __construct(Controller $controller, LockFactory $factory, UserHandler $handler, string $name = null)
     {
+        $this->factory = $factory;
         $this->controller = $controller;
         $this->handler = $handler;
         parent::__construct($name);
@@ -52,7 +56,12 @@ class AccessControlCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         try {
+            $lock = $this->factory->createLock('azure-ad-delta-sync');
+            $lock->acquire(true);
+
             $this->controller->run($this->handler);
+
+            $lock->release();
         } catch (DataException | NetworkException | TokenException $e) {
             throw $e;
         }
